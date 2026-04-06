4275ef768a7abeed3516102e3b58153f954401fa
Habeeb O. Issa <habeebissa023@gmail.com>
2026-04-06 14:59:18 +0100

    Hyperparameter Tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -35,7 +35,7 @@ class DataIngestion:
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
-            train_set,test_set = train_test_split(df,test_size=0.2,random_state=23)
+            train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
             logging.info('Train-test-split initiated')
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
